57a9fdb8ea74ff5030f44b15ad6b5a5ae85b70bf
pigeongcc <48735488+pigeongcc@users.noreply.github.com>
2020-08-10 14:07:13 +0300

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    float x1,y1,x2,y2,x3,y3, l1, l2, l3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+    l1 = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+    l2 = sqrt((x2-x3)*(x2-x3) + (y2-y3)*(y2-y3));
+    l3 = sqrt((x3-x1)*(x3-x1) + (y3-y1)*(y3-y1));
+
+    if (l1+l2 <= l3 || l1+l3 <= l2 || l3+l2 <= l1){
+                cout << "Triangle doesn't exist.";
+                return 0;
+              }
+    cout << "Triangle does exist.";
+    return 0;
+}
